7c13cb29672075aa21d1f45180a0882c1fbf420b
GOUTHAM MERTHIPATI <gouthammerthipati31@gmail.com>
2026-09-16 16:05:43 +0530

    Add grade calculation

--- student_marks.py
+++ student_marks.py
@@ -6,3 +6,14 @@ average = sum(marks) / len(marks)
 print("Student:", name)
 print("Marks:", marks)
 print("Average:", average)
+
+if average >= 90:
+    grade = "A"
+elif average >= 80:
+    grade = "B"
+elif average >= 70:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade:", grade)
